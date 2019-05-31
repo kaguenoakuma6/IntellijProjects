21233710dd596865e31ec6a5bf686970fdc28cd3
shiro <shadowfocker666@gmail.com>
2019-05-31 18:31:52 -0500

    AES 256 encription complete;

--- src/com/Utilidades/AES256Utilities.java
+++ src/com/Utilidades/AES256Utilities.java
@@ -1,34 +1,113 @@
 package com.Utilidades;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.UUID;
 
 public class AES256Utilities
 {
-    public static void main(String[] args)
+    private static String algorit = "AES";
+    private static String cifra = "AES/ECB/PKCS5Padding";
+    private static byte[] key;
+
+    private static Cipher cipher;
+    private static SecretKeySpec secretKey;
+
+    public static void setKey(String secret)
     {
         try
         {
-            String token = "sIYphQHsMUY=";
-            String desecBase = "KiWtFzIb5gFCU4H0cotzBQ==", desenc ="";
-
-            //SecretKeySpec keySpec = new SecretKeySpec(token.getBytes(), "AES");
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding" );
-
-            //cipher.init(Cipher.DECRYPT_MODE, keySpec);
-
-
-            //desenc = new String(cipher.doFinal(Base64.getDecoder().decode(desecBase)));
-
-            //System.out.println(desenc);
-            desenc = new String(Base64.getDecoder().decode(token.getBytes("UTF-8")));
-            System.out.println(desenc);
+            key = secret.getBytes("UTF-8");
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, algorit);
 
         }
-        catch (Exception e )
+        catch (UnsupportedEncodingException e)
         {
             e.printStackTrace();
         }
+    }
+
+
+    public static void setCipher()
+    {
+        try
+        {
+            cipher = Cipher.getInstance(cifra);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static String encripta(String textCifra)
+    {
+        String encripted ="";
+
+        try
+        {
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            encripted = Base64.getEncoder().encodeToString(cipher.doFinal(textCifra.getBytes("UTF-8")));
+        }
+        catch (InvalidKeyException e)
+        {
+            e.printStackTrace();
+        } catch (BadPaddingException e)
+        {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e)
+        {
+            e.printStackTrace();
+        }
+
+        return encripted;
+    }
+
+    public static String desencripta(String textDesenc)
+    {
+        String desenc = "";
+
+        try
+        {
+            cipher.init(Cipher.DECRYPT_MODE,secretKey);
+            desenc = new String(cipher.doFinal(Base64.getDecoder().decode(textDesenc)));
+        }
+        catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException e)
+        {
+            e.printStackTrace();
+        }
+        return desenc;
+    }
+
+    public static void main(String[] args)
+    {
+
+        //String token = "sIYphQHsMUY=";
+        String token = "576ab1c2b7f646508daa0448661d779c";
+        //String token1 = UUID.randomUUID().toString().replace("-", "");
+        String desecBase = "KiWtFzIb5gFCU4H0cotzBQ==", desenc = "", encript1 = "hola mundo", strEncript = "", strDesencript = "";
+        //desenc = new String(Base64.getDecoder().decode(token));
+        setKey(token);
+        setCipher();
+
+        strEncript = encripta(encript1);
+        strDesencript = desencripta(strEncript);
+
+        System.out.println(token);
+        //System.out.println(desenc);
+        System.out.println(strEncript);
+        System.out.println(strDesencript);
+
+
     }
 }
